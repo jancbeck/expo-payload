b69d210c95bedbcde24a5f8d063e8280a2902943
Jan Beck <jancbeck@users.noreply.github.com>
2024-10-24 01:04:46 +0200

    make it work

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,14 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    overrides: [
+      {
+        test: [/@payloadcms/, /payload/, /prettier/],
+        plugins: [
+          "babel-plugin-transform-import-meta",
+          "module:@reactioncommerce/babel-remove-es-create-require",
+        ],
+      },
+    ],
   };
 };
