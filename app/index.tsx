4f2ecec8a21006492b9ff3ea7f5fc58b1998bfc7
Jan Beck <jancbeck@users.noreply.github.com>
2024-10-26 21:11:11 +0200

    remove fun

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,4 @@
-import { Suspense } from "react";
 import { Text, View } from "react-native";
-import { getPayload } from "@/lib/payload";
 import { Form } from "@/components/Forms";
 
 export default function HomePage() {
@@ -9,24 +7,7 @@ export default function HomePage() {
       <Text style={{ fontWeight: "bold", fontSize: 24 }}>
         Node: {process.version}
       </Text>
-      <Suspense fallback={<Text>Loading...</Text>}>
-        <Posts />
-      </Suspense>
       <Form />
-    </View>
-  );
-}
-
-async function Posts() {
-  const payload = await getPayload();
-  const { docs } = await payload.find({
-    collection: "posts",
-  });
-  return !!docs.length ? (
-    docs.map((post) => <Text key={post.id}>{post.title}</Text>)
-  ) : (
-    <View>
-      <Text>No posts yet.</Text>
     </View>
   );
 }
