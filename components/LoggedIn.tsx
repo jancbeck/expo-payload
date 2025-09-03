d798e0a863b1c2e4fc184addd65057a004acd172
Jan Beck <865311+jancbeck@users.noreply.github.com>
2025-09-03 20:32:30 +0200

    enable full react server components mode

--- components/LoggedIn.tsx
+++ components/LoggedIn.tsx
@@ -1,24 +1,28 @@
 "use client";
 
 import { Text } from "react-native";
-import { Redirect } from "expo-router";
+import { useRouter } from "expo-router";
 
 import { useSession } from "@/components/Providers";
+import { useEffect } from "react";
 
 export function LoggedIn({ children }: { children: React.ReactNode }) {
   const { session, isLoading } = useSession();
+  const router = useRouter();
+
+  // Only require authentication within the (app) group's layout as users
+  // need to be able to access the (auth) group and sign in again.
+  useEffect(() => {
+    if (!session) {
+      // On web, static rendering will stop here as the user is not authenticated
+      // in the headless Node process that the pages are rendered in.
+      return router.push("/");
+    }
+  });
 
   // You can keep the splash screen open, or render a loading screen like we do here.
   if (isLoading) {
     return <Text>Loading...</Text>;
-  }
-
-  // Only require authentication within the (app) group's layout as users
-  // need to be able to access the (auth) group and sign in again.
-  if (!session) {
-    // On web, static rendering will stop here as the user is not authenticated
-    // in the headless Node process that the pages are rendered in.
-    return <Redirect href="/" />;
   }
 
   // This layout can be deferred because it's not the root layout.
