e4a1bb7fdeabb7e9cc0d516b9c2a49c7578ce6f6
Jan Beck <865311+jancbeck@users.noreply.github.com>
2025-09-03 22:19:37 +0200

    setup eslint and prettier

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
@@ -8,11 +8,11 @@ const config = getDefaultConfig(__dirname);
 // Add custom resolver to handle problematic packages
 config.resolver.resolveRequest = (context, moduleName, platform) => {
   // Specifically intercept uuid imports
-  if (moduleName === "uuid") {
+  if (moduleName === 'uuid') {
     return {
       // payload dependency drizzle uses uuid v9. Resolve to v10 instead.
-      filePath: path.resolve(__dirname, "./node_modules/uuid/dist/index.js"),
-      type: "sourceFile",
+      filePath: path.resolve(__dirname, './node_modules/uuid/dist/index.js'),
+      type: 'sourceFile',
     };
   }
 
