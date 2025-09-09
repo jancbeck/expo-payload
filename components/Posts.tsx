c4255cbe2d8b35e890b9c079be97b7d07a3d029a
Jan Beck <865311+jancbeck@users.noreply.github.com>
2025-09-09 14:13:38 +0200

    add comment

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -9,6 +9,7 @@ export function Posts() {
   const authCookie = getCookie();
 
   // Call the Server Function with token for authentication
+  // Causes > ERROR  Cannot update a component (`%s`) while rendering a different component (`%s`).
   const posts = useMemo(() => renderPosts({ authCookie }), [authCookie]);
   return <Suspense fallback={<Text>Loading...</Text>}>{posts}</Suspense>;
 }
